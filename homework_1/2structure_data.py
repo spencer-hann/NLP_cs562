e13641d7ce3e794f100724def1e471a85bdeaea9
spencer <spencer.hann@gmail.com>
2019-01-14 12:13:44 -0800

    changed basic output to pickle

--- homework_1/2structure_data.py
+++ homework_1/2structure_data.py
@@ -2,6 +2,7 @@ import sys
 import re
 from nltk.tokenize import sent_tokenize, word_tokenize
 from string import punctuation
+import pickle
 
 
 if __name__ == "__main__":
@@ -13,17 +14,18 @@ if __name__ == "__main__":
         print("reading file: " + sys.argv[1], file=sys.stderr)
         f = f.read()
 
-        print("removing newlines", file=sys.stderr)
-        f = re.sub(r'\n|\r', ' ', f)
+    print("removing newlines", file=sys.stderr)
+    f = re.sub(r'\n|\r', ' ', f)
 
-        print("tokenizing sentences", file=sys.stderr)
-        tokens = sent_tokenize(f)
-        print("number of sentences: " + str(len(tokens)), file=sys.stderr)
+    print("tokenizing sentences", file=sys.stderr)
+    tokens = sent_tokenize(f)
+    print("number of sentences: " + str(len(tokens)), file=sys.stderr)
 
-        print("removing punctuation and tokenizing words", file=sys.stderr)
-        table = str.maketrans(dict.fromkeys(punctuation))
-        for i, t in enumerate(tokens):
-            tokens[i] = t.translate(table) # remove punctuation
-            tokens[i] = word_tokenize(tokens[i])
+    print("removing punctuation and tokenizing words", file=sys.stderr)
+    table = str.maketrans(dict.fromkeys(punctuation))
+    for i, t in enumerate(tokens):
+        tokens[i] = t.translate(table) # remove punctuation
+        tokens[i] = word_tokenize(tokens[i])
 
-        for token in tokens: print(token)
+    with open("p2_output.txt", "wb") as out:
+        pickle.dump(tokens, out)
