c6ea2685bfe688254dafb981181b6985fddad1af
spencer <spencer.hann@gmail.com>
2019-01-14 18:10:25 -0800

    separated script for getting n-grams so that it only has to be done once

---
+++ homework_1/3ngrams.py
@@ -0,0 +1,41 @@
+import sys
+import dill
+import pickle
+from collections import defaultdict
+
+# this is necessary because for some reason
+# defaultdict(lamda:defaultdict(int)) can't be pickled
+def _dd():
+    return defaultdict(int)
+
+def get_ngrams(tokens):
+    unigrams = defaultdict(int)
+    bigrams = defaultdict(_dd)
+
+    for sentence in tokens:
+        s = [None] + sentence + [None]
+        for i in range(1, len(s)):
+            unigrams[s[i]] += 1
+            bigrams[s[i-1]][s[i]] += 1
+
+    del unigrams[None]
+    return unigrams, bigrams
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("ERROR: pass in exactly one txt file to be parsed for token list.")
+
+    with open(sys.argv[1], "rb") as f:
+        print("loading pickle from:", sys.argv[1], file=sys.stderr)
+        tokens = pickle.load(f)
+
+    print("creating n-grams", file=sys.stderr)
+    unigrams, bigrams = get_ngrams(tokens)
+    print(type(unigrams))
+    print(type(bigrams))
+
+    print("pickling...", file=sys.stderr)
+    with open("p3_unigrams_output.txt", "wb") as f:
+        pickle.dump(unigrams, f)
+    with open("p3_bigrams_output.txt", "wb") as f:
+        pickle.dump(bigrams, f)
